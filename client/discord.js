8c67a1ceaf212970d6fedf12dcfcd1dffedbdf1e
Tommy Lo <mantolo@hotmail.com>
2024-11-03 18:02:08 +0800

    Apply discord oauth2 authorize logic

--- client/discord.js
+++ client/discord.js
@@ -1,7 +1,51 @@
 import { DiscordSDK } from "@discord/embedded-app-sdk";
 
 const discordSdk = new DiscordSDK(import.meta.env.VITE_DISCORD_CLIENT_ID);
+// Will eventually store the authenticated user's access_token
+
+/**
+ * @type {null | Awaited<ReturnType<DiscordSDK["commands"]["authenticate"]>>}
+ */
+let auth = null;
 
 export async function setupDiscordSdk() {
   await discordSdk.ready();
+
+  // Authorize with Discord Client
+  const { code } = await discordSdk.commands.authorize({
+    client_id: import.meta.env.VITE_DISCORD_CLIENT_ID,
+    response_type: "code",
+    state: "",
+    prompt: "none",
+    scope: ["identify", "guilds", "applications.commands"],
+  });
+
+  // Retrieve an access_token from your activity's server
+  // Note: We need to prefix our backend `/api/token` route with `/.proxy` to stay compliant with the CSP.
+  // Read more about constructing a full URL and using external resources at
+  // https://discord.com/developers/docs/activities/development-guides#construct-a-full-url
+  const response = await fetch("/.proxy/api/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      code,
+    }),
+  });
+  const { access_token } = await response.json();
+
+  // Authenticate with Discord client (using the access_token)
+  auth = await discordSdk.commands.authenticate({
+    access_token,
+  });
+
+  if (auth === null) {
+    throw new Error("Authenticate command failed");
+  }
 }
+
+/**
+ * Get Auth instance
+ */
+export const getAuth = () => auth;
--- client/main.js
+++ client/main.js
@@ -1,14 +1,24 @@
-import { setupDiscordSdk } from "./discord";
+import { setupDiscordSdk, getAuth } from "./discord";
 import "./style.css";
 import rocketLogo from "/rocket.png";
 
 setupDiscordSdk().then(() => {
   console.log("Discord SDK is ready");
+
+  document.querySelector("#app").innerHTML = `
+<div>
+  <img src="${rocketLogo}" class="logo" alt="Discord" />
+  <h1>Hello, World! :)</h1>
+  <p>Logged in as ${getAuth()?.user.username}#${
+    getAuth()?.user.discriminator
+  }</p>
+  <p>With access token: ${getAuth()?.access_token}</p>
+  <p>With Application: ${getAuth()?.application.name}#${
+    getAuth()?.application.id
+  }</p>
+</div>`;
 });
 
 document.querySelector("#app").innerHTML = `
-  <div>
-    <img src="${rocketLogo}" class="logo" alt="Discord" />
-    <h1>Hello, World!</h1>
-  </div>
+  Authorizing with Discord...
 `;
